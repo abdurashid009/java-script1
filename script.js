7ae686bcc7a44ecebd3a055633e0c1bfbd4f3b22
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-01-08 17:06:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let user = prompt('Sening isming nima?');
+let box = prompt('Sening familiyang nima?');
+let item = prompt('Sening yoshing nechada?');
+let admin = prompt('Sen qayerdansan?');
+let print = prompt('Sen qaysi fandga qiziqasan?');
+
+alert(`Salom mening ismim ${user},Familiyam ${box} meni yoshim,${item}da ,Men ${admin}likman,men ${print} ga qiziqaman `)
